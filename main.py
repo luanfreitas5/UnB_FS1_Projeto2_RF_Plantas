ea3e6d2ff571742f68fc452c17f0c6daecb44d42
luanfreitas5 <luan.mgf@gmail.com>
2023-02-15 21:28:08 -0300

    update

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # Nome: Luan Mendes Gonçalves Freitas
-# Matricula: 15/0015585
 # Disciplina: Fundamentos de Sistemas Inteligentes
 # Projeto 2 - Florestas Randômicas
 # Módulo main
 
-from floresta_150015585 import FlorestaRandomica
+from floresta import FlorestaRandomica
 
 def main():
     floresta = FlorestaRandomica()
